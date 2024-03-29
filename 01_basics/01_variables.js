57614b6db5fe28ca8380262444bd005862535fd9
MD TAUQUEER REHAN <107019913+tauqeer7328@users.noreply.github.com>
2024-03-29 18:04:56 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId=23413565;
+var name="rehan7328"
+let accountname="saving"
+accountSalary=30000
+// accountId=123456
+console.log(accountId)
+console.table([accountId,accountname,accountSalary])
